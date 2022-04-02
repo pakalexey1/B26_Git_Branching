b012ef0f416afe169c6961943ac964c46556e807
Veronica Pak <nikampak@gmail.com>
2022-04-02 11:55:58 -0400

    Added negative testing for TC001

--- src/test_cases/TC001.java
+++ src/test_cases/TC001.java
@@ -2,8 +2,11 @@ package test_cases;
 
 public class TC001 {
     public static void main(String[] args) {
-        //testing some thing
+        // testing something
 
-        //more testing
+        // more testing
+
+        // negative testing
+
     }
 }
